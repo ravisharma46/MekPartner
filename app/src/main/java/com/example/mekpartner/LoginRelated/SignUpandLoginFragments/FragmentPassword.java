cd30dce6aac5c325cce085717917e683ca64534e
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-23 21:30:50 +0530

    Create FragmentPassword.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/SignUpandLoginFragments/FragmentPassword.java
@@ -0,0 +1,34 @@
+package com.example.mekpartner.LoginRelated.SignUpandLoginFragments;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mekpartner.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentPassword extends Fragment {
+
+
+    public FragmentPassword() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_fragment_password, container, false);
+
+    }
+
+}
