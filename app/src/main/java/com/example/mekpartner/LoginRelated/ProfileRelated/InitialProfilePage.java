4500a90b7fbea62988d630b9c3b1e24c9c0c1541
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-23 21:30:43 +0530

    Create InitialProfilePage.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/ProfileRelated/InitialProfilePage.java
@@ -0,0 +1,16 @@
+package com.example.mekpartner.LoginRelated.ProfileRelated;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mekpartner.R;
+
+public class InitialProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_profile_page);
+    }
+}
