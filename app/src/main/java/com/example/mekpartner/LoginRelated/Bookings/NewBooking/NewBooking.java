a98e65968ab712bace965d35dedf51ec19bbf1a0
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-25 20:49:40 +0530

    Update NewBooking.java

--- app/src/main/java/com/example/mekpartner/LoginRelated/Bookings/NewBooking/NewBooking.java
+++ app/src/main/java/com/example/mekpartner/LoginRelated/Bookings/NewBooking/NewBooking.java
@@ -4,6 +4,8 @@ package com.example.mekpartner.LoginRelated.Bookings.NewBooking;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +13,15 @@ import android.view.ViewGroup;
 
 import com.example.mekpartner.R;
 
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewBooking extends Fragment {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
 
     public NewBooking() {
         // Required empty public constructor
@@ -26,7 +32,37 @@ public class NewBooking extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_booking, container, false);
+        View v= inflater.inflate(R.layout.fragment_new_booking, container, false);
+
+
+        ArrayList<NewBookingDataClass> list = initData();
+
+
+
+
+
+        recyclerView = (RecyclerView)v.findViewById(R.id.recyclerView1);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        adapter = new NewBookingAdapter(list);
+        recyclerView.setAdapter(adapter);
+
+
+        return v;
     }
+
+    private ArrayList<NewBookingDataClass> initData() {
+        ArrayList<NewBookingDataClass> list = new ArrayList<>();
+        list.add(new NewBookingDataClass("Awaiting confirmation","Denting Painting"));
+        list.add(new NewBookingDataClass("Awaiting confirmation","Emergency Towing"));
+        list.add(new NewBookingDataClass("Awaiting confirmation","Flat Tyre"));
+        list.add(new NewBookingDataClass("Awaiting confirmation","Onsite assistance"));
+
+
+
+        return list;
+    }
+
 
 }
