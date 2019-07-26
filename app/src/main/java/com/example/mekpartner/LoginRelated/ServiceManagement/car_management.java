04230efd19d2f90a7bae5dc0a152f20bab4ffb07
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-26 21:06:15 +0530

    Create car_management.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/ServiceManagement/car_management.java
@@ -0,0 +1,32 @@
+package com.example.mekpartner.LoginRelated.ServiceManagement;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.mekpartner.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class car_management extends Fragment {
+
+
+    public car_management() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_car_management, container, false);
+    }
+
+}
