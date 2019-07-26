6213cbd33174496d14c7b80b5b3975f4b0a17008
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-26 21:06:23 +0530

    Create sosBike.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/ServiceManagement/sosBike.java
@@ -0,0 +1,25 @@
+package com.example.mekpartner.LoginRelated.ServiceManagement;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mekpartner.R;
+
+public class sosBike extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sos_bike, container, false);
+    }
+
+
+}
