df5bf5853bcf21ac0b9dfd08cdab5ee4d8679ebc
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-26 21:06:12 +0530

    Create bike_fragment.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/ServiceManagement/bike_fragment.java
@@ -0,0 +1,33 @@
+package com.example.mekpartner.LoginRelated.ServiceManagement;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mekpartner.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class bike_fragment extends Fragment {
+
+
+    public bike_fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bike_management, container, false);
+    }
+
+}
