51c0666f8c7e2b08bf8885a5c1b6303f0a327694
Aayush <aayushaggarwalt20>
2019-07-27 16:12:35 +0530

    commit by ayush

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/AddToCart/SwipeControllerActions.java
@@ -0,0 +1,9 @@
+package com.example.vendorapp.AddToCart;
+
+public abstract class SwipeControllerActions {
+
+    public void onLeftClicked(int position) {}
+
+    public void onRightClicked(int position) {}
+
+}
