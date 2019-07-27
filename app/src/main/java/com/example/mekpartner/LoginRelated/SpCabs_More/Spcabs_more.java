fb35d17a54d31d1e16ec53581c33d970de02fdfe
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-27 15:06:53 +0530

    Create Spcabs_more.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/SpCabs_More/Spcabs_more.java
@@ -0,0 +1,113 @@
+package com.example.mekpartner.LoginRelated.SpCabs_More;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.MenuItem;
+
+import com.example.mekpartner.LoginRelated.NavDrawer.NavActivity;
+import com.example.mekpartner.LoginRelated.SpCabs_More.History.History_sp_fragment;
+import com.example.mekpartner.LoginRelated.SpCabs_More.NewBooking.New_sp_fragment;
+import com.example.mekpartner.LoginRelated.SpCabs_More.UpcomingBooking.Upcoming_sp_fragment;
+import com.example.mekpartner.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spcabs_more extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spcabs_more);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#000000'>Cabs & More</font>"));
+        final Drawable upArrow = getResources().getDrawable(R.drawable.ic_keyboard_backspace_black_24dp);
+        upArrow.setColorFilter(getResources().getColor(R.color.chart_deep_red), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+
+        setTabLayout();
+    }
+
+    private void setTabLayout(){
+
+        viewPager = findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        NavActivity.ViewPagerAdapter adapter = new NavActivity.ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new New_sp_fragment(), "New");
+        adapter.addFragment(new Upcoming_sp_fragment(), "Upcoming");
+        adapter.addFragment(new History_sp_fragment(), "History");
+        viewPager.setAdapter(adapter);
+    }
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
