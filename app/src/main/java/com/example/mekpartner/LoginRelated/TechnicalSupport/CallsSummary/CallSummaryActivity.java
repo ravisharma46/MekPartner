7b1a4cacc98f8fdde0cef0f150ba3879c8d053c0
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-27 11:19:58 +0530

    Create CallSummaryActivity.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/TechnicalSupport/CallsSummary/CallSummaryActivity.java
@@ -0,0 +1,16 @@
+package com.example.mekpartner.LoginRelated.TechnicalSupport.CallsSummary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mekpartner.R;
+
+public class CallSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_summary);
+    }
+}
