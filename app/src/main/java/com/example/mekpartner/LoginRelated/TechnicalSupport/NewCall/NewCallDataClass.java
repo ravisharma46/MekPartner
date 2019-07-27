19ff8a5f412a14ce0ff30281083c1b238937ed3a
RAVI SHARMA <ravi.ravi.sharma46@gmail.com>
2019-07-27 11:20:14 +0530

    Create NewCallDataClass.java

---
+++ app/src/main/java/com/example/mekpartner/LoginRelated/TechnicalSupport/NewCall/NewCallDataClass.java
@@ -0,0 +1,17 @@
+package com.example.mekpartner.LoginRelated.TechnicalSupport.NewCall;
+
+public class NewCallDataClass {
+
+    String mobileNo;
+    public  NewCallDataClass(String m){
+        this.mobileNo=m;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+}
